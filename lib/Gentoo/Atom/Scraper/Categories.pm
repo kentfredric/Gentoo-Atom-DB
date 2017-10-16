65086b9e89f4a4a3b0c185c472e7f664e863325e
Kent Fredric <kentnl@gentoo.org>
2017-10-16 15:07:29 +1300

    Add iterator

--- lib/Gentoo/Atom/Scraper/Categories.pm
+++ lib/Gentoo/Atom/Scraper/Categories.pm
@@ -14,12 +14,25 @@ use Path::Tiny qw( path );
 
 sub scrape {
     my ( $self, $repo ) = @_;
-    my $path = path($repo);
-    $path->exists or die "$path should be a repository path";
-    my $catfile = $path->child( 'profiles', 'categories' );
-    $catfile->is_file or die "$catfile should exist and be a category listing";
-    return [ sort grep { $path->child($_)->is_dir }
-          $catfile->lines_raw( { chomp => 1 } ) ];
+    my (@out);
+    $self->foreach( $repo, sub {
+      push @out, $_[0]
+    });
+    return [ sort @out ];
+}
+
+sub foreach {
+  my ( $self, $repo, $callback ) = @_;
+  my $path = path($repo);
+  $path->exists or die "$path should be a repository path";
+  my $catfile = $path->child( 'profiles', 'categories' );
+  $catfile->is_file or die "$catfile should exist and be a category listing";
+  my $fh = $catfile->openr_raw;
+  while( my $line = <$fh> ) {
+    chomp $line;
+    next unless $path->child($line)->is_dir;
+    $callback->( $line );
+  }
 }
 
 1;
--- xt/author/01-scrape-categories.t
+++ xt/author/01-scrape-categories.t
@@ -14,4 +14,17 @@ ok( exists $scrape{'dev-perl'},  "category 'dev-perl' scraped" );
 ok( !exists $scrape{'profiles'}, "profiles is not a category" );
 ok( !exists $scrape{'metadata'}, "metadata is not a category" );
 
+my %found;
+Gentoo::Atom::Scraper::Categories->foreach(
+    $ENV{PORTDIR} || '/usr/portage',
+    sub {
+        $_[0] eq 'dev-perl' and $found{'dev-perl'}++;
+        $_[0] eq 'virtual'  and $found{'virtual'}++;
+        $_[0] eq 'profiles' and $found{'profiles'}++;
+        $_[0] eq 'metadata' and $found{'metadata'}++;
+    }
+);
+cmp_ok( scalar keys %found,
+    '==', 2, "Found exactly all the looked for categories" );
+
 done_testing;
