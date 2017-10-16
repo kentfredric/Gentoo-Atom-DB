f10daf5cfbb3fba38647d450779b70f6a9680a58
Kent Fredric <kentnl@gentoo.org>
2017-10-16 15:32:49 +1300

    Add category iterator

--- lib/Gentoo/Atom/Scraper/Packages.pm
+++ lib/Gentoo/Atom/Scraper/Packages.pm
@@ -15,20 +15,61 @@ use Path::Tiny qw( path );
 
 sub scrape {
     my ( $self, $repository ) = @_;
-    return [ map { $self->scrape_category_full($_) }
-          @{ Gentoo::Atom::Scraper::Categories->scrape($repository) } ];
+    my (@out);
+    $self->foreach(
+        $repository => sub {
+            push @out, "$_[0]/$_[1]";
+        }
+    );
+    return [ sort @out ];
 }
 
 sub scrape_category_full {
     my ( $self, $repository, $category ) = @_;
-    return [ map { "$category/$_" }
-          @{ $self->scrape_category_short( $repository, $category ) } ];
+    my @out;
+    $self->foreach_category(
+        $repository => $category => sub {
+            push @out, "$category/$_[0]";
+        }
+    );
+    return [ sort @out ];
 }
 
 sub scrape_category_short {
     my ( $self, $repository, $category ) = @_;
-    return [ sort map { $_->basename }
-          grep { $_->is_dir } path( $repository, $category )->children ];
+    my @out;
+    $self->foreach_category(
+        $repository,
+        $category,
+        sub {
+            push @out, $_[0];
+        }
+    );
+    return [ sort @out ];
+}
+
+sub foreach {
+    my ( $self, $repository, $callback ) = @_;
+    Gentoo::Atom::Scraper::Categories->foreach(
+        $repository => sub {
+            my $category = $_[0];
+            $self->foreach_category(
+                $repository => $category => sub {
+                    $callback->( $category, $_[0] );
+                }
+            );
+        }
+    );
+}
+
+sub foreach_category {
+    my ( $self, $repository, $category, $callback ) = @_;
+    opendir my $dfh, path( $repository, $category )->absolute->stringify;
+    while ( my $nodename = readdir $dfh ) {
+        next if $nodename eq '.' or $nodename eq '..';
+        next unless path( $repository, $category, $nodename )->is_dir;
+        $callback->($nodename);
+    }
 }
 
 1;
